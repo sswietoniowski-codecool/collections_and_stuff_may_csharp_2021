e618707282cf02bd91cfdf43f3700b2a59c99b0c
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-12-11 12:38:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace collections_and_stuff_may_csharp_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
